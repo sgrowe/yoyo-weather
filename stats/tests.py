dfab63faca1df18bf367b6fc650e76e62dddc2db
sam <sgrowe@live.co.uk>
2016-08-03 22:29:31 +0100

    Add ability to query forecast api and caculate basic statistics

--- stats/tests.py
+++ stats/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from stats.views import get_latitude_and_longitude
+from stats.views import get_latitude_and_longitude, WeatherData, get_forecast_data
 
 
 class GeocodingTests(TestCase):
@@ -14,3 +14,34 @@ class GeocodingTests(TestCase):
     def test_get_latitude_and_longitude_returns_none_if_address_invalid(self):
         address = "lskjflkfjhsdlk"
         self.assertIsNone(get_latitude_and_longitude(address))
+
+
+class WeatherDataTests(TestCase):
+    def test_calculates_correct_statistics(self):
+        data = {
+            'daily': {
+                'data': [{'humidity': h} for h in [47.5, 13.2, 20.6]],
+            }
+        }
+        weather_data = WeatherData(data)
+        stats = weather_data._get_stats_for('humidity')
+        self.assertAlmostEqual(stats['mean'], 27.1)
+        self.assertEqual(stats['min'], 13.2)
+        self.assertEqual(stats['max'], 47.5)
+        self.assertEqual(stats['median'], 20.6)
+
+    def test_returns_valid_serialised_data(self):
+        data = {
+            'daily': {
+                'data': [{'humidity': h, 'temperatureMax': t} for h, t in [(47.5, 21.0), (13.2, 4.5), (20.6, 33.3)]],
+            }
+        }
+        weather_data = WeatherData(data)
+        serialiser = weather_data.get_serialiser()
+        self.assertTrue(serialiser.is_valid())
+
+
+class GetForecastDataTests(TestCase):
+    def test_returns_weather_data_object(self):
+        data = get_forecast_data(37.8267, -122.423)
+        self.assertIsInstance(data, WeatherData)
--- stats/views.py
+++ stats/views.py
@@ -1,6 +1,8 @@
+from rest_framework import serializers
 from rest_framework.decorators import api_view
+from urllib.parse import quote as url_quote
+from statistics import mean, median
 import requests
-
 
 GEOCODING_API_KEY = "AIzaSyD6uu4X9WOw2-hjcFrhooEuev2YBLCgZ_k"
 
@@ -18,3 +20,53 @@ def get_latitude_and_longitude(address):
     else:
         raise ValueError
 
+
+FORECAST_API_KEY = "edf73a3d61144d6a104356f2b53bf5b9"
+
+
+def get_forecast_data(latitude, longitude):
+    latitude, longitude = url_quote(str(latitude)), url_quote(str(longitude))
+    url = 'https://api.forecast.io/forecast/{}/{},{}'.format(FORECAST_API_KEY, latitude, longitude)
+    response = requests.get(url)
+    if response.status_code == 200:
+        return WeatherData(response.json())
+
+
+class WeatherData:
+    def __init__(self, json):
+        self.json = json
+
+    def _get_daily_data_for(self, item):
+        for day in self.json['daily']['data']:
+            yield day[item]
+
+    def _get_stats_for(self, item):
+        data = list(self._get_daily_data_for(item))
+        return {
+            'median': median(data),
+            'mean': mean(data),
+            'min': min(data),
+            'max': max(data),
+        }
+
+    def _get_serialised_data_for(self, item):
+        return StatisticsSerialiser(data=self._get_stats_for(item))
+
+    def get_serialiser(self):
+        data = {
+            'humidity': self._get_stats_for('humidity'),
+            'temperature': self._get_stats_for('temperatureMax'),
+        }
+        return WeatherDataSerialiser(data=data)
+
+
+class StatisticsSerialiser(serializers.Serializer):
+    mean = serializers.FloatField()
+    median = serializers.FloatField()
+    min = serializers.FloatField()
+    max = serializers.FloatField()
+
+
+class WeatherDataSerialiser(serializers.Serializer):
+    humidity = StatisticsSerialiser()
+    temperature = StatisticsSerialiser()
