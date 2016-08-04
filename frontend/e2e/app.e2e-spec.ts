af2b9c79f1882bcbea517b5ad160d9b2bde95eec
sam <sgrowe@live.co.uk>
2016-08-04 20:04:08 +0100

    Switch to using angular cli rather than the quickstart project (easier to produce builds)

--- frontend/e2e/app.e2e-spec.ts
+++ frontend/e2e/app.e2e-spec.ts
@@ -1,15 +1,14 @@
+import { FrontendPage } from './app.po';
 
-describe('QuickStart E2E Tests', function () {
+describe('frontend App', function() {
+  let page: FrontendPage;
 
-  let expectedMsg = 'My First Angular 2 App';
-
-
-  beforeEach(function () {
-    browser.get('');
+  beforeEach(() => {
+    page = new FrontendPage();
   });
 
-  it('should display: ' + expectedMsg, function () {
-    expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
   });
-
 });
